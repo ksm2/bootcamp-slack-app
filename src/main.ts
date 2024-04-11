de92724eacc37bf9337bc2e4cc49ed2e4228310e
Konstantin Möllers <ksm.moellers@gmail.com>
2024-04-11 14:19:49 +0200

    Setup Express server

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
+import express from "express";
 import { Application } from "./application/Application.js";
 
 const application = new Application();
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("OK");
+});
+
 await application.start();
+
+app.listen(8080, () => {
+  console.log("App running on http://localhost:8080");
+});
